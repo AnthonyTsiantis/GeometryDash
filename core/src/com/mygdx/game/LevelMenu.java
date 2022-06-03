c37ae9c22d23b6923edf8c84ee337b6968e68e82
AnthonyTsiantis <63810804+AnthonyTsiantis@users.noreply.github.com>
2022-06-02 22:47:57 -0400

    Completed Level Menu

--- core/src/com/mygdx/game/LevelMenu.java
+++ core/src/com/mygdx/game/LevelMenu.java
@@ -8,18 +8,32 @@ import com.badlogic.gdx.graphics.Texture;
 public class LevelMenu implements Screen {
     Boot game;
 
-    Texture levelMenuBackground;
-    Texture backButtonActive;
-    Texture backButtonInactive;
+    private Texture levelMenuBackground;
+    private Texture level1Off, level1On;
+    private Texture level2Off, level2On;
+    private Texture level3Off, level3On;
+    private Texture level4Off, level4On;
+    private Texture backButtonActive;
+    private Texture backButtonInactive;
 
     private static final int BACK_BUTTON_WIDTH = 150;
     private static final int BACK_BUTTON_HEIGHT = 75;
+    private static final int BUTTON_WIDTH = 400;
+    private static final int BUTTON_HEIGHT = 200;
 
     public LevelMenu(Boot game) {
         this.game = game;
-        levelMenuBackground = new Texture("Menu/background.jpeg");
-        backButtonActive = new Texture("Menu/Back_Button_On.png");
-        backButtonInactive = new Texture("Menu/Back_Button_Off.png");
+        this.levelMenuBackground = new Texture("Menu/Levels Menu/levels background.png");
+        this.level1Off = new Texture("Menu/Levels Menu/level 1 Off.png");
+        this.level1On = new Texture("Menu/Levels Menu/level 1 On.png");
+        this.level2Off = new Texture("Menu/Levels Menu/level 2 Off.png");
+        this.level2On = new Texture("Menu/Levels Menu/level 2 On.png");
+        this.level3Off = new Texture("Menu/Levels Menu/level 3 Off.png");
+        this.level3On = new Texture("Menu/Levels Menu/level 3 On.png");
+        this.level4Off = new Texture("Menu/Levels Menu/level 4 Off.png");
+        this.level4On = new Texture("Menu/Levels Menu/level 4 On.png");
+        this.backButtonActive = new Texture("Menu/Back_Button_On.png");
+        this.backButtonInactive = new Texture("Menu/Back_Button_Off.png");
     }
 
     @Override
@@ -54,6 +68,54 @@ public class LevelMenu implements Screen {
             }
         } else {
             game.batch.draw(backButtonInactive, backButtonHorizontalOffset, backButtonVerticalOffset, BACK_BUTTON_WIDTH, BACK_BUTTON_HEIGHT);
+        }
+
+        //new GameScreen(this.game, this.game.camera)
+
+        int verticalOff = 350;
+        int horizontalOff = (this.game.widthScreen / 2) + 100;
+        if ((mouseX > 150) && (mouseX < 150 + BUTTON_WIDTH) && (mouseY < game.heightScreen - verticalOff) && (mouseY > game.heightScreen - (verticalOff + BUTTON_HEIGHT))) {
+            this.game.batch.draw(this.level1On, 150, verticalOff, BUTTON_WIDTH, BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.game.levelNum = 1;
+                this.dispose();
+                this.game.setScreen(new GameScreen(this.game, this.game.camera));
+            }
+        } else {
+            this.game.batch.draw(this.level1Off, 150, verticalOff, BUTTON_WIDTH, BUTTON_HEIGHT);
+        }
+
+        if ((mouseX > horizontalOff) && (mouseX < horizontalOff + BUTTON_WIDTH) && (mouseY < game.heightScreen - verticalOff) && (mouseY > game.heightScreen - (verticalOff + BUTTON_HEIGHT))) {
+            this.game.batch.draw(this.level2On, horizontalOff, verticalOff, BUTTON_WIDTH, BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.game.levelNum = 2;
+                this.dispose();
+                this.game.setScreen(new GameScreen(this.game, this.game.camera));
+            }
+        } else {
+            this.game.batch.draw(this.level2Off, horizontalOff, verticalOff, BUTTON_WIDTH, BUTTON_HEIGHT);
+        }
+
+        if ((mouseX > 150) && (mouseX < 150 + BUTTON_WIDTH) && (mouseY < game.heightScreen - 125) && (mouseY > game.heightScreen - (125 + BUTTON_HEIGHT))) {
+            this.game.batch.draw(this.level3On, 150, 125, BUTTON_WIDTH, BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.game.levelNum = 3;
+                this.dispose();
+                this.game.setScreen(new GameScreen(this.game, this.game.camera));
+            }
+        } else {
+            this.game.batch.draw(this.level3Off, 150, 125, BUTTON_WIDTH, BUTTON_HEIGHT);
+        }
+
+        if ((mouseX > horizontalOff) && (mouseX < horizontalOff + BUTTON_WIDTH) && (mouseY < game.heightScreen - 125) && (mouseY > game.heightScreen - (125 + BUTTON_HEIGHT))) {
+            this.game.batch.draw(this.level4On, horizontalOff, 125, BUTTON_WIDTH, BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.game.levelNum = 4;
+                this.dispose();
+                this.game.setScreen(new GameScreen(this.game, this.game.camera));
+            }
+        } else {
+            this.game.batch.draw(this.level4Off, horizontalOff, 125, BUTTON_WIDTH, BUTTON_HEIGHT);
         }
         game.batch.end();
     }
